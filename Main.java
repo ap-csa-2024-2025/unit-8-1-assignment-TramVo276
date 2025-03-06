d5fe8f685b8424ef2df0f7fb37c8607f0eacc70c
TramVo276 <tvo941352@gmail.com>
2025-03-06 10:16:31 -0800

    sfb

--- Main.java
+++ Main.java
@@ -4,17 +4,45 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
-  }
+    int [] [] mat = productTable(5, 5);
+
+    for (int [] row : mat)
+    {
+      for (int elem : row)
+      {
+        
+      }
+    }
+      }
 
 
   public static int sumOfDiag(int[][] matrix)
   {
-    return 0;
+    int sum = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[row].length; col++)
+      {
+        if (row == col) //main diagonal
+        {
+          sum+= mat[row][col];
+        }
+      }
+    }
+
+    return sum;
   }
 
   public static int[][] productTable(final int numRows, final int numCols)
   {
-    return null;
+    int [] [] mat = new int[numRows][numCols];
+
+    for (int r = 0; r < numRows; r++)
+    {
+      for (int c = 0; c < numCols; numColsc++)
+      {
+        r * c;
+      }
+    return sum;
   }
 }
